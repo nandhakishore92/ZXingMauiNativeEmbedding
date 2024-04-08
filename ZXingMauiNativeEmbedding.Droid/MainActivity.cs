f3c8a4780b4785c29446c011b7075c91a29aeb96
Nantha Kishor <nantha.kishor@amcsgroup.com>
2024-04-08 14:56:13 +0100

    Simplified under 1 project.

--- ZXingMauiNativeEmbedding.Droid/MainActivity.cs
+++ ZXingMauiNativeEmbedding.Droid/MainActivity.cs
@@ -1,38 +1,23 @@
 using Microsoft.Maui.Embedding;
+using Microsoft.Maui.Platform;
 using ZXing.Net.Maui.Controls;
-using ZXingMauiNativeEmbedding.MauiPages;
 
 namespace ZXingMauiNativeEmbedding.Droid
 {
 	[Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/Theme.MaterialComponents.Light.DarkActionBar")]
-    public class MainActivity : Activity
-    {
-		public static readonly Lazy<MauiApp> MauiApp = new(() =>
+	public class MainActivity : Activity
+	{
+		MauiContext mauiContext;
+		protected override void OnCreate(Bundle? savedInstanceState)
 		{
-			var mauiApp = MauiProgram.CreateMauiApp(builder =>
-			{
-				builder
-					.UseMauiEmbedding()
-					.UseBarcodeReader();
-			});
-			return mauiApp;
-		});
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			// Ensure .NET MAUI app is built before creating .NET MAUI views
-			var mauiApp = MauiApp.Value;
-
-			// Create .NET MAUI context
-			var mauiContext = new MauiContext(mauiApp.Services, this);  // Create app context
-			var mauiView = new MainPage();
-			var scannerView = mauiView.ToPlatformEmbedded(mauiContext);
-			SetContentView(scannerView);
-		}
-
-		public override void OnBackPressed()
-		{
-			MoveTaskToBack(true);
+			base.OnCreate(savedInstanceState);
+			var builder = MauiApp.CreateBuilder();
+			builder.UseMauiEmbedding<Microsoft.Maui.Controls.Application>().UseBarcodeReader();
+			var mauiApp = builder.Build();
+			mauiContext = new MauiContext(mauiApp.Services, this);
+			var myMauiPage = new MainPage();
+			var containerView = myMauiPage.ToContainerView(mauiContext);
+			SetContentView(containerView);
 		}
 	}
 }
